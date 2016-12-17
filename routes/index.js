cb78ed1591550b9a7e756af26228674be2f1b533
kvisner <G0neSkiing!>
2016-12-16 18:05:55 -0800

    Testing Dynamo wiring

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var AWS = require("aws-sdk");
+var shortid = require('shortid');
 
-/* GET home page. */
+var docClient = new AWS.DynamoDB.DocumentClient();
+AWS.config.update({endpoint: "https://dynamodb.us-west-2.amazonaws.com"});
+var table = "ShortenedURL";
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'MiniLinx' });
 });
+
+router.post('/', function (req, res) {
+  var id = shortid.generate()
+  res.send('URL id = ' + id)
+  var params = {
+    TableName:table,
+    Item:{
+      "webid": id,
+      "url":"test"
+    }
+  };
+  docClient.put(params, function(err, data) {
+    if (err) {
+      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+  });
+})
 
 module.exports = router;
