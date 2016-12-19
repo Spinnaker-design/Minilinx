29bb6a945ed37fca589d25919bf00210c88e45e7
kvisner <G0neSkiing!>
2016-12-19 01:27:15 -0800

    Fixed code style

--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,55 @@
-var express = require('express');
+var express = require( "express" );
 var router = express.Router();
-const url = require('url');
-var awsAdapter = require('../helpers/awsAdapter')
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MiniLinx' });
-});
+const url = require( "url" );
+var awsAdapter = require( "../helpers/awsAdapter" );
+router.get( "/", function( req, res, next ) {
+  res.render( "index", { title: "MiniLinx" } );
+} );
 
-router.get('/details/:id', function(req, res, next) {
+router.get( "/details/:id", function( req, res, next ) {
   var webid = req.params.id;
-  awsAdapter.getUrlData(webid).on('success', function(response) {
-    if(response.data.Item){
-      res.render('details', { title: 'MiniLinx', webid: response.data.Item.webid, url: response.data.Item.url});
+  awsAdapter.getUrlData( webid ).on( "success", function( response ) {
+    if ( response.data.Item ) {
+      res.render( "details",
+      {
+        title: "MiniLinx",
+        webid: response.data.Item.webid, url:
+        response.data.Item.url
+      } );
+    } else {
+      res.status( 404 ).render( "error", { error: "No Such URL" } );
     }
-    else{
-      res.status(404).render('error', { error: 'No Such URL' })
-    }
-  }).on('error', function(response) {
-    res.status(500).render('error', { error: 'Something Broke!' })
-  })
-});
+  } ).on( "error", function( response ) {
+    res.status( 500 ).render( "error", { error: "Something Broke!" } );
+  } );
+} );
 
-router.get('/:id', function(req, res, next) {
+router.get( "/:id", function( req, res, next ) {
   var webid = req.params.id;
-  awsAdapter.getUrlData(webid).on('success', function(response) {
-    if(response.data.Item){
-      res.redirect(response.data.Item.url);
+  awsAdapter.getUrlData( webid ).on( "success", function( response ) {
+    if ( response.data.Item ) {
+      res.redirect( response.data.Item.url );
+    } else {
+      res.status( 404 );
+      res.render( "error", { error: "No Such URL" } );
     }
-    else{
-      res.status(404)
-      res.render('error', { error: 'No Such URL' })
-    }
-  }).on('error', function(response) {
-    res.status(500).render('error', { error: 'Somthing Broke!' })
-  })
-});
+  } ).on( "error", function( response ) {
+    res.status( 500 ).render( "error", { error: "Somthing Broke!" } );
+  } );
+} );
 
-router.post('/', function (req, res) {
-  awsAdapter.saveUrlData(req.body.url).on('success', function(response) {
-    res.redirect('/details/' + response.request.params.Item.webid.S)
-  }).on('error', function(response) {
-    res.status(500)
-  })
-})
+router.post( "/", function( req, res ) {
+  awsAdapter.saveUrlData( req.body.url ).on( "success", function( response ) {
+    res.redirect( "/details/" + response.request.params.Item.webid.S );
+  } ).on( "error", function( response ) {
+    res.status( 500 );
+  } );
+} );
 
-router.post('/inflate', function (req, res) {
-  console.log(req.body)
-  var id = req.body.shortUrl.substr(req.body.shortUrl.lastIndexOf('/') + 1);
-  res.redirect('/details/' + id)
-})
+router.post( "/inflate", function( req, res ) {
+  console.log( req.body );
+  var id = req.body.shortUrl.substr( req.body.shortUrl.lastIndexOf( "/" ) + 1 );
+  res.redirect( "/details/" + id );
+} );
 
 module.exports = router;
--- test/tests.js
+++ test/tests.js
@@ -1,101 +1,101 @@
-const Browser = require('zombie');
+const Browser = require( "zombie" );
 
 // We're going to make requests to http://example.com/signup
 // Which will be routed to our test server localhost:3000
-Browser.localhost('localhost', 3000);
+Browser.localhost( "localhost", 3000 );
 
-describe('User visits url form page', function() {
+describe( "User visits url form page", function() {
 
   const browser = new Browser();
 
-  before(function(done) {
-    browser.visit('/', done);
-  });
+  before( function( done ) {
+    browser.visit( "/", done );
+  } );
 
-  describe('submits shorten form', function() {
+  describe( "submits shorten form", function() {
 
-    before(function(done) {
+    before( function( done ) {
       browser
-      .fill('url',    'http://www.google.com')
-      .pressButton('Shorten', done);
-    });
+      .fill( "url",    "http://www.google.com" )
+      .pressButton( "Shorten", done );
+    } );
 
-    it('should be successful', function() {
+    it( "should be successful", function() {
       browser.assert.success();
-    });
+    } );
 
-    it('should see detail page', function() {
-      browser.assert.text('[name="url"]', 'http://www.google.com');
-    });
-  });
-});
+    it( "should see detail page", function() {
+      browser.assert.text( '[name="url"]', "http://www.google.com" );
+    } );
+  } );
+} );
 
-describe('User visits url form page', function() {
+describe( "User visits url form page", function() {
 
   const browser = new Browser();
 
-  before(function(done) {
-    browser.visit('/', done);
-  });
-  describe('submits detail form', function() {
+  before( function( done ) {
+    browser.visit( "/", done );
+  } );
+  describe( "submits detail form", function() {
 
-    before(function(done) {
+    before( function( done ) {
       browser
-      .fill('shortUrl',    'http://node-express-env.mpw7wpz2uk.us-west-2.elasticbeanstalk.com/H1BfmiVEl')
-      .pressButton('Details', done);
-    });
+      .fill( "shortUrl", "http://node-express-env.mpw7wpz2uk.us-west-2.elasticbeanstalk.com/H1BfmiVEl" )
+      .pressButton( "Details", done );
+    } );
 
-    it('should be successful', function() {
+    it( "should be successful", function() {
       browser.assert.success();
-    });
+    } );
 
-    it('should see detail page', function() {
-      browser.assert.text('[name="url"]', 'http://www.google.com');
-    });
-  });
-});
+    it( "should see detail page", function() {
+      browser.assert.text( '[name="url"]', "http://www.google.com" );
+    } );
+  } );
+} );
 
-describe('User visits url form page', function() {
+describe( "User visits url form page", function() {
 
   const browser = new Browser();
 
-  before(function(done) {
-    browser.visit('/', done);
-  });
-  describe('submits detail form with bad data', function() {
-    it('should NOT be successful', function() {
+  before( function( done ) {
+    browser.visit( "/", done );
+  } );
+  describe( "submits detail form with bad data", function() {
+    it( "should NOT be successful", function() {
       browser
-      .fill('shortUrl', 'http://node-express-env.mpw7wpz2uk.us-west-2.elasticbeanstalk.com/SomeJunk')
-      .pressButton('Details', function(error){
-        browser.assert.status(404);
-      });
-    });
-  });
-});
+      .fill( "shortUrl", "http://node-express-env.mpw7wpz2uk.us-west-2.elasticbeanstalk.com/SomeJunk" )
+      .pressButton( "Details", function( error ) {
+        browser.assert.status( 404 );
+      } );
+    } );
+  } );
+} );
 
-describe('User visits shortened url', function() {
+describe( "User visits shortened url", function() {
 
   const browser = new Browser();
 
-  before(function(done) {
-    browser.visit('/H1BfmiVEl', done);
-  });
-  it('should be successful', function() {
+  before( function( done ) {
+    browser.visit( "/H1BfmiVEl", done );
+  } );
+  it( "should be successful", function() {
     browser.assert.redirected();
-  });
-  it('should be the full url', function() {
-    browser.assert.url('http://www.google.com')
-  });
-});
+  } );
+  it( "should be the full url", function() {
+    browser.assert.url( "http://www.google.com" );
+  } );
+} );
 
-describe('User visits bad shortened url', function() {
+describe( "User visits bad shortened url", function() {
 
   const browser = new Browser();
 
-  it('should NOT be successful', function(done) {
-    browser.visit('/SomeJunk', function(error){
-      browser.assert.status(404);
-      done()
-    });
-  });
-});
+  it( "should NOT be successful", function( done ) {
+    browser.visit( "/SomeJunk", function( error ) {
+      browser.assert.status( 404 );
+      done();
+    } );
+  } );
+} );
